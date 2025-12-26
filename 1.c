94c0456e31b5516c5e44a1ee92fb1c4f2ae52ceb
captain34643875-wq <captain34643875@gmail.com>
2025-12-26 22:12:58 +0900

    테스트

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+   int i = 0, j = 0;
+   for(i = 2; i <= 100; i++){
+      for(j = 2; j < i; j++){
+         if(i % j == 0)
+            break;
+      }
+      if(i == j)
+         printf("%d ", i);
+   }
+   printf("\n");
+   return 0;
+}
